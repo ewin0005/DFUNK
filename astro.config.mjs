ba9f10b2bba77990e050b31a29403e00bbab7973
ewin0005 <120107248+ewin0005@users.noreply.github.com>
2024-08-11 19:10:39 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+
+
+////       this is were integrations can be added      ////
+// https://astro.build/config
+export default defineConfig({
+  // I added the tailwind css integration in order to keep track and have some sort of documentation on the css
+
+});
